8861f423ca3ade60a8f9b761c9104a7607cdb397
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-10-23 23:39:43 +0000

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    GameObject player;
+    Rigidbody enemyRb;
+    public float speed=5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveDirection = player.transform.position - transform.position;
+        enemyRb.AddForce(moveDirection.normalized * speed);
+    }
+}
